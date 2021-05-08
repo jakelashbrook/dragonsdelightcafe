096ccc519b28035b5bdd99eb462d20aa54b2c5e0
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-05-08 16:25:54 +0000

    Form testing

---
+++ assets/js/email.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('request-table-btn');
+
+document.getElementById('reservation-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'gmail';
+   const templateID = 'table-reserve';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Thank you for your Table Reservation Request, you will recieve a confirmation email from us within a few minutes. We will ONLY contact you if their is an issue with your reservation request!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
